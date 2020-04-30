c3e3f2d3ecda66921144ed6b43e04d60505c1539
Maksim <msurguy@gmail.com>
2020-04-29 23:08:02 -0700

    updates to UI

--- src/App.vue
+++ src/App.vue
@@ -5,59 +5,161 @@
       <div class="sidebar">
         <div class="controls-wrapper">
           <div class="controls">
-            <p class="mt-3 lead text-center text-white"> Source </p>
             <div class="sidebar-control">
               <div class="control-header">
                 <div></div>
                 <div class="control-label">
-                  Upload File
+                  Choose SVG File
                 </div>
                 <div></div>
               </div>
               <div class="custom-file">
-                <input @change="handleFileUpload" type="file" ref="file" accept="image/svg+xml"
-                       class="custom-file-input"
-                       id="sourcefile">
-                <label class="custom-file-label" for="sourcefile">Upload SVG file</label>
+                <input
+                  id="sourcefile"
+                  type="file"
+                  accept="image/svg+xml"
+                  class="custom-file-input"
+                  @change="handleFileUpload">
+                <label
+                  class="custom-file-label"
+                  for="sourcefile">Upload SVG file</label>
               </div>
-              <button class="btn btn-primary btn-sm" @click="submitFile">Submit</button>
+              <button
+                class="btn btn-primary btn-sm"
+                @click="submitFile">
+                Upload
+              </button>
+
+              <div
+                v-if="source.loading"
+                class="progress">
+                <div
+                  class="progress-bar"
+                  role="progressbar"
+                  aria-valuenow="100"
+                  aria-valuemin="0"
+                  aria-valuemax="100"
+                  style="width: 100%">
+                  Uploading SVG
+                </div>
+              </div>
             </div>
 
-            <div class="progress" v-if="source.loading">
-              <div class="progress-bar" role="progressbar"
-                   aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%">Loading SVG
+            <div class="sidebar-control">
+              <div class="zoom-controls">
+                <span class="small">zoom: <kbd>alt</kbd> + <kbd>mousewheel</kbd></span>
+                <div class="buttons">
+                  <button
+                    class="btn btn-sm"
+                    @click="zoomIn">
+                    <svg
+                      id="i-zoom-in"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 32 32"
+                      width="16"
+                      height="16"
+                      fill="none"
+                      stroke="currentcolor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2">
+                      <circle
+                        cx="14"
+                        cy="14"
+                        r="12" />
+                      <path d="M23 23 L30 30" />
+                      <path d="M14 10 L14 18 M10 14 L18 14" />
+                    </svg>
+                  </button>
+                  <button
+                    class="btn btn-sm"
+                    @click="zoomOut">
+                    <svg
+                      id="i-zoom-out"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 32 32"
+                      width="16"
+                      height="16"
+                      fill="none"
+                      stroke="currentcolor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2">
+                      <circle
+                        cx="14"
+                        cy="14"
+                        r="12" />
+                      <path d="M23 23 L30 30" />
+                      <path d="M10 14 L18 14" />
+                    </svg>
+                  </button>
+                  <button
+                    class="btn btn-sm pr-0"
+                    @click="resetZoom">
+                    <svg
+                      id="i-zoom-reset"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 32 32"
+                      width="16"
+                      height="16"
+                      fill="none"
+                      stroke="currentcolor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2">
+                      <circle
+                        cx="14"
+                        cy="14"
+                        r="12" />
+                      <path d="M23 23 L30 30" />
+                      <path d="M9 12 L9 9 12 9 M16 9 L19 9 19 12 M9 16 L9 19 12 19 M19 16 L19 19 16 19" />
+                    </svg>
+                  </button>
+                </div>
               </div>
             </div>
 
             <transition name="slide">
-              <div v-if="source.svg">
-               
+              <div v-if="optimizedSVG">
+                <text-input
+                  v-model="strokeWidth"
+                  label="Stroke Width"></text-input>
               </div>
             </transition>
-
           </div>
         </div>
 
-        <div class="button">
+        <!-- <div class="button">
           <div class="reveal"></div>
-          <button class="btn btn-primary btn-block" @click.prevent="download">
+          <button
+            class="btn btn-primary btn-block"
+            @click.prevent="download">
             Download SVG
           </button>
-        </div>
+        </div> -->
       </div>
 
       <!-- Page Content -->
       <div class="paper">
-        <div id="sketch" class="sketch">
-          <div id="svg-wrapper" ref="svgWrapper">
+        <div
+          id="sketch"
+          class="sketch">
+          <div
+            id="svg-wrapper"
+            ref="svgWrapper"
+            v-html="optimizedSVG">
           </div>
         </div>
       </div>
       <div class="footer-wrapper">
         <div class="footer">
           <h3>SVG Checker</h3>
-          <p>Project by <a target="_blank" href="http://twitter.com/msurguy">@msurguy</a> (<a target="_blank"
-                                                                                              href="http://github.com/msurguy/cnc-text-tool">Source</a>)
+          <p>
+            Project by <a
+              target="_blank"
+              href="http://twitter.com/msurguy">@msurguy</a> (<a
+                target="_blank"
+                href="http://github.com/msurguy/cnc-text-tool">Source</a>)
           </p>
         </div>
       </div>
@@ -66,130 +168,165 @@
 </template>
 
 <script>
-  import bsCustomFileInput from 'bs-custom-file-input'
+import bsCustomFileInput from 'bs-custom-file-input'
+import panzoom from 'panzoom'
+// import Toggle from '@/components/Toggle'
+import TextInput from '@/components/TextInput'
+// import { downloadSVG } from '@/util/download'
 
-  import Toggle from "@/components/Toggle";
-  import SelectField from "@/components/SelectField";
-  import Slider from "@/components/Slider";
-  // import ButtonGroup from "@/components/ButtonGroup";
-  import TextAreaInput from "@/components/TextAreaInput";
-  import DoubleTextInput from "@/components/DoubleTextInput";
-
-  import { downloadSVG } from "@/util/download";
-
-  export default {
-    name: 'Rooot',
-    components: {
-      Toggle,
-      SelectField,
-      Slider,
-      TextAreaInput,
-      DoubleTextInput
+export default {
+  name: 'Rooot',
+  components: {
+    TextInput
+  },
+  data () {
+    return {
+      file: '',
+      strokeWidth: '0.2mm',
+      optimizedSVG: null,
+      source: {
+        element: null,
+        loading: false,
+        width: 0,
+        height: 0,
+        viewbox: '0 0 0 0'
+      },
+      panzoom: null
+    }
+  },
+  watch: {
+    strokeWidth (val) {
+      this.source.element.setAttribute('stroke-width', val)
+    }
+  },
+  mounted () {
+    bsCustomFileInput.init()
+  },
+  methods: {
+    zoomIn () {
+      this.panzoom.smoothZoom(0, 0, 1.5)
     },
-    props: {
-      msg: String
+    zoomOut () {
+      this.panzoom.smoothZoom(0, 0, 0.5)
     },
-    data () {
-      return {
-        file: '',
-        source: {
-          filename: '',
-          svg: '',
-          string: '',
-          loading: false,
-          width: 0,
-          height: 0,
-          viewbox: '0 0 0 0'
-        }
-      }
+    resetZoom () {
+      this.panzoom.moveTo(0, 0)
+      this.panzoom.zoomAbs(0, 0, 1)
     },
-    mounted () {
-      bsCustomFileInput.init();
-    },
-    methods: {
-        /*
+    /*
         Submits the file to the server
       */
-      submitFile(){
-        /*
+    submitFile () {
+      this.source.loading = true
+      /*
                 Initialize the form data
             */
-            let formData = new FormData();
-
-            /*
-                Add the form data we need to submit
-            */
-            formData.append('file', this.file);
-            /*
-              Additional POST Data
-            */
-            // formData.append('first_name', 'Dan');
-            // formData.append('last_name', 'Pastori');
-        /*
-          Make the request to the POST /single-file URL
-        */
-            this.axios.post( 'https://svg-optimizer-api.msurguy.now.sh/api/optimize',
-                formData,
-                {
-                headers: {
-                  'Access-Control-Allow-Origin': '*',
-                    'Content-Type': 'multipart/form-data; charset=utf-8;'
-                }
-              }
-            ).then(function(response){
-          console.log(response);
-        })
-        .catch(function(error){
-          console.log(error);
-        });
-      },
+      const formData = new FormData()
 
       /*
+                Add the form data we need to submit
+            */
+
+      // TODO: optimize SVG by cutting out precision before sending
+      formData.append('file', this.file)
+      /*
+              Additional POST Data
+            */
+      const optionsJSON = {
+        addDefaultFillStroke: true,
+        precision: 2,
+        removeDimensions: false
+      }
+      formData.append('flags', JSON.stringify(optionsJSON))
+      this.axios.post('https://svg-optimizer-api.msurguy.now.sh/api/optimize',
+        formData,
+        {
+          headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Content-Type': 'multipart/form-data; charset=utf-8;'
+          }
+        }
+      ).then(({ data }) => {
+        this.optimizedSVG = data
+        // Update dimensions of the SVG here
+        // Get width and height from ViewBox if not present otherwise!
+        this.source.loading = false
+        this.$nextTick(() => {
+          this.source.element = this.$refs.svgWrapper.firstChild
+          const renderedSVG = this.source.element
+          // add width and height if they're missing
+          if (renderedSVG.getAttribute('viewBox') && !renderedSVG.getAttribute('width') && !renderedSVG.getAttribute('height')) {
+            const dimensions = renderedSVG.getAttribute('viewBox').split(/[ ,]+/g)
+            renderedSVG.setAttribute('width', dimensions[2])
+            renderedSVG.setAttribute('height', dimensions[3])
+          }
+          this.strokeWidth = '0.2mm'
+
+          this.panzoom = panzoom(this.source.element, {
+            smoothScroll: false,
+            beforeWheel: function (e) {
+              // allow wheel-zoom only if altKey is down. Otherwise - ignore
+              var shouldIgnore = !e.altKey
+              return shouldIgnore
+            }
+          })
+        })
+      })
+        .catch((error) => {
+          console.log(error)
+          this.source.loading = false
+        })
+    },
+
+    /*
         Handles a change on the file upload
       */
-      handleFileUpload(){
-        this.file = this.$refs.file.files[0];
-      },
-      // onSourceFileChange (e) {
-      //   this.source.loading = true;
-      //   // reset position / scale / other params of the text
-      //   let files = e.target.files || e.dataTransfer.files;
-      //   if (!files.length)
-      //     return;
-      //   this.readSourceImage(files[0]);
-      // },
-      // readSourceImage (file) {
-      //   const reader = new FileReader();
-      //   reader.onload = async (e) => {
-      //     // Set file name
-      //     this.source.filename = file.name.substr(0, file.name.lastIndexOf('.'));
+    handleFileUpload (e) {
+      const files = e.target.files || e.dataTransfer.files
+      if (!files.length) { return }
 
-      //     // try finding "<svg" in the document:
-      //     let fileContents = e.target.result;
-      //     this.processInputSVG(fileContents)
-      //   };
-      //   reader.readAsText(file);
-      // },
-      // async processInputSVG (fileContents) {
-      //   console.log(fileContents)
-      // },
-      download () {
-        // downloadSVG(this.$refs.svgWrapper.childNodes[1], flattenedGroup, `line-text-${Date.now()}`)
+      if (this.panzoom) { this.panzoom.dispose() }
+
+      const reader = new FileReader()
+      reader.onload = (e) => {
+        this.file = new Blob([e.target.result.replace(/([+]?\d+\.\d{3,}([eE][+]?\d+)?)/g, (x) => (+x).toFixed(2))], { type: 'image/svg+xml;charset=utf-8' })
       }
+      reader.readAsText(files[0])
+    },
+    // onSourceFileChange (e) {
+    //   this.source.loading = true;
+    //   // reset position / scale / other params of the text
+    //   let files = e.target.files || e.dataTransfer.files;
+    //   if (!files.length)
+    //     return;
+    //   this.readSourceImage(files[0]);
+    // },
+    // readSourceImage (file) {
+    //   const reader = new FileReader();
+    //   reader.onload = async (e) => {
+    //     // Set file name
+    //     this.source.filename = file.name.substr(0, file.name.lastIndexOf('.'));
+
+    //     // try finding "<svg" in the document:
+    //     let fileContents = e.target.result;
+    //     this.processInputSVG(fileContents)
+    //   };
+    //   reader.readAsText(file);
+    // },
+    // async processInputSVG (fileContents) {
+    //   console.log(fileContents)
+    // },
+    download () {
+      // downloadSVG(this.$refs.svgWrapper.childNodes[1], flattenedGroup, `line-text-${Date.now()}`)
     }
   }
+}
 </script>
 
 <style>
 
   #app {
     height: 100%;
-  }
-
-  .canvas {
-    position: absolute;
-    left: 0;
-    top: 0;
   }
 
   #svg-wrapper rect {
@@ -204,6 +341,13 @@
     position: relative;
     height: 100%;
     display: flex;
+  }
+
+  .zoom-controls {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
   }
 
   .controls {
@@ -240,6 +384,7 @@
   }
 
   .sketch {
+    z-index: 1;
     position: relative;
     /*overflow: auto;*/
   }
@@ -248,6 +393,7 @@
     position: relative;
     display: inline-block;
     border: 1px dashed #000;
+    overflow: hidden;
   }
 
   .sidebar {
@@ -312,4 +458,3 @@
     overflow: hidden;
   }
 </style>
-
--- src/components/TextInput.vue
+++ src/components/TextInput.vue
@@ -2,18 +2,37 @@
   <div class="sidebar-control">
     <div class="control-header">
       <div>
-        <svgicon v-if="leftIcon" :name="leftIcon.icon" :width="leftIcon.width" :height="leftIcon.height" color="#FFFFFF" :fill="false"></svgicon>
+        <svgicon
+          v-if="leftIcon"
+          :name="leftIcon.icon"
+          :width="leftIcon.width"
+          :height="leftIcon.height"
+          color="#FFFFFF"
+          :fill="false"></svgicon>
       </div>
       <div class="control-label">
         {{ label }}
       </div>
       <div>
-        <svgicon v-if="rightIcon" :name="rightIcon.icon" :width="rightIcon.width" :height="rightIcon.height" color="#FFFFFF" :fill="false"></svgicon>
+        <svgicon
+          v-if="rightIcon"
+          :name="rightIcon.icon"
+          :width="rightIcon.width"
+          :height="rightIcon.height"
+          color="#FFFFFF"
+          :fill="false"></svgicon>
       </div>
     </div>
     <div class="control-header">
-      <input class="control-text-input" type="text" :value="value" @change="onInput">
-      <a v-if="showReset" class="btn" @click="$emit('reset')">reset</a>
+      <input
+        class="control-text-input"
+        type="text"
+        :value="value"
+        @change="onInput">
+      <a
+        v-if="showReset"
+        class="btn"
+        @click="$emit('reset')">reset</a>
     </div>
   </div>
 </template>
@@ -25,10 +44,12 @@ export default {
   name: 'TextInput',
   props: {
     leftIcon: {
-      type: Object
+      type: Object,
+      default: () => {}
     },
     rightIcon: {
-      type: Object
+      type: Object,
+      default: () => {}
     },
     label: {
       type: String,
